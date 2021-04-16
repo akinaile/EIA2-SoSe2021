0ee3c99a574bffd501dc4dcb8cc65d3bed5e34c2
akinaile <aileen-akin@web.de>
2021-04-16 17:08:05 +0200

    L02 EventInspector

---
+++ A02_EventInspector/script.js
@@ -0,0 +1,26 @@
+var A02_EventInspector;
+(function (A02_EventInspector) {
+    console.log("start");
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        document.addEventListener("mousemove", setInfoBox);
+        document.addEventListener("click", logInfo);
+        document.addEventListener("keyup", logInfo);
+    }
+    function setInfoBox(_event) {
+        var x = _event.pageX;
+        var y = _event.pageY;
+        var span = document.querySelector("span");
+        var position = span;
+        position.style.left = x + "px";
+        position.style.top = y + "px";
+        position.textContent = "x-Koordinate" + x + "px" + " , " + "y-Koordinate" + y + "px";
+    }
+    function logInfo(_event) {
+        console.log(_event);
+        console.log(_event.target);
+        console.log(_event.currentTarget);
+        console.log(_event.type);
+    }
+})(A02_EventInspector || (A02_EventInspector = {}));
+//# sourceMappingURL=script.js.map
